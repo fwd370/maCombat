91b2f5341d154ce109f354019be9519c49be56de
fwd370 <fwd1@berkeley.edu>
2022-11-11 18:05:46 +0800

    finalise TL run.sh

--- 5runRL.sh
+++ 5runRL.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 nohup $(\
-	python vdn.py --gamma 0.90 --member wd --run 152 --max-epsilon 0.6 --batch-size 32 --chunk-size 15 --target-interval 20 --max-episodes 10000;\
-	python vdn.py --gamma 0.90 --member wd --run 153 --max-epsilon 0.5 --batch-size 32 --chunk-size 15 --target-interval 20 --max-episodes 10000;\
-	python vdn.py --gamma 0.90 --member wd --run 154 --max-epsilon 0.75 --batch-size 32 --chunk-size 15 --target-interval 20 --max-episodes 10000\
+	python vdn_tl_noop_s2.py --gamma 0.90 --member wd --run 1000 --max-epsilon 0.6 --batch-size 32 --chunk-size 15 --target-interval 50 --max-episodes 7500;\
+	python vdn_tl_noop_s2.py --gamma 0.90 --member wd --run 1001 --max-epsilon 0.5 --batch-size 32 --chunk-size 15 --target-interval 50 --max-episodes 7500;\
+	python vdn_tl_noop_s2.py --gamma 0.90 --member wd --run 1002 --max-epsilon 0.4 --batch-size 32 --chunk-size 15 --target-interval 50 --max-episodes 7500\
 	) &
 nohup $(\
-	python vdn.py --gamma 0.90 --member wd --run 155 --max-epsilon 0.6 --batch-size 32 --chunk-size 15 --target-interval 50 --max-episodes 10000;\
-	python vdn.py --gamma 0.90 --member wd --run 156 --max-epsilon 0.5 --batch-size 32 --chunk-size 15 --target-interval 50 --max-episodes 10000;\
-	python vdn.py --gamma 0.90 --member wd --run 157 --max-epsilon 0.75 --batch-size 32 --chunk-size 15 --target-interval 50 --max-episodes 10000\
+	python vdn_tl_noop_s2.py --gamma 0.90 --member wd --run 1003 --max-epsilon 0.7 --batch-size 32 --chunk-size 15 --target-interval 50 --max-episodes 7500;\
+	python vdn_tl_noop_s2.py --gamma 0.90 --member wd --run 1004 --max-epsilon 0.8 --batch-size 32 --chunk-size 15 --target-interval 50 --max-episodes 7500;\
+	python vdn_tl_noop_s2.py --gamma 0.90 --member wd --run 1005 --max-epsilon 0.9 --batch-size 32 --chunk-size 15 --target-interval 50 --max-episodes 7500\
 	) &
 nohup $(\
-	python vdn.py --gamma 0.90 --member wd --run 158 --max-epsilon 0.6 --batch-size 32 --chunk-size 10 --target-interval 20 --max-episodes 10000;\
-	python vdn.py --gamma 0.90 --member wd --run 159 --max-epsilon 0.5 --batch-size 32 --chunk-size 10 --target-interval 20 --max-episodes 10000;\
-	python vdn.py --gamma 0.90 --member wd --run 160 --max-epsilon 0.75 --batch-size 32 --chunk-size 10 --target-interval 20 --max-episodes 10000\
+	python vdn_tl_noop_s2.py --gamma 0.90 --member wd --run 1006 --max-epsilon 0.3 --batch-size 32 --chunk-size 15 --target-interval 50 --max-episodes 7500;\
+	python vdn_tl_noop_s2.py --gamma 0.90 --member wd --run 1007 --max-epsilon 0.2 --batch-size 32 --chunk-size 15 --target-interval 50 --max-episodes 7500;\
+	python vdn_tl_noop_s2.py --gamma 0.90 --member wd --run 1008 --max-epsilon 0.1 --batch-size 32 --chunk-size 15 --target-interval 50 --max-episodes 7500\
 	) &
